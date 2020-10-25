fcb5597719be391ad7e12322ac8ce56b77bded48
virgantara <oddy@unida.gontor.ac.id>
2020-10-25 12:30:36 +0700

    remove unused code

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import numpy as np
 import pandas as pd
 
 import os
-import librosa
 
-import scipy
-from scipy.stats import skew
-from tqdm import tqdm, tqdm_pandas
+from tqdm import tqdm
 
 tqdm.pandas()
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 from sklearn.svm import SVC
+
+from mfcc_extraction import get_mfcc, convert_to_labels
 
 audio_train_files = os.listdir('dataset/audio_train')
 audio_test_files = os.listdir('dataset/audio_test')
@@ -23,37 +22,7 @@ audio_test_files = os.listdir('dataset/audio_test')
 train = pd.read_csv('dataset/train.csv')
 submission = pd.read_csv('dataset/test.csv')
 
-SAMPLE_RATE = 44100
-def get_mfcc(name, path):
-    data, _ = librosa.core.load(path + name, sr = SAMPLE_RATE)
-    assert _ == SAMPLE_RATE
-    try:
-        ft1 = librosa.feature.mfcc(data, sr = SAMPLE_RATE, n_mfcc=30)
-        ft2 = librosa.feature.zero_crossing_rate(data)[0]
-        ft3 = librosa.feature.spectral_rolloff(data)[0]
-        ft4 = librosa.feature.spectral_centroid(data)[0]
-        ft5 = librosa.feature.spectral_contrast(data)[0]
-        ft6 = librosa.feature.spectral_bandwidth(data)[0]
-        ft1_trunc = np.hstack((np.mean(ft1, axis=1), np.std(ft1, axis=1), skew(ft1, axis = 1), np.max(ft1, axis = 1), np.median(ft1, axis = 1), np.min(ft1, axis = 1)))
-        ft2_trunc = np.hstack((np.mean(ft2), np.std(ft2), skew(ft2), np.max(ft2), np.median(ft2), np.min(ft2)))
-        ft3_trunc = np.hstack((np.mean(ft3), np.std(ft3), skew(ft3), np.max(ft3), np.median(ft3), np.min(ft3)))
-        ft4_trunc = np.hstack((np.mean(ft4), np.std(ft4), skew(ft4), np.max(ft4), np.median(ft4), np.min(ft4)))
-        ft5_trunc = np.hstack((np.mean(ft5), np.std(ft5), skew(ft5), np.max(ft5), np.median(ft5), np.min(ft5)))
-        ft6_trunc = np.hstack((np.mean(ft6), np.std(ft6), skew(ft6), np.max(ft6), np.median(ft6), np.max(ft6)))
-        return pd.Series(np.hstack((ft1_trunc, ft2_trunc, ft3_trunc, ft4_trunc, ft5_trunc, ft6_trunc)))
-    except:
-        print('bad file')
-        return pd.Series([0]*210)
 
-def convert_to_labels(preds, i2c, k=3):
-    ans = []
-    ids = []
-    for p in preds:
-        idx = np.argsort(p)[::-1]
-        ids.append([i for i in idx[:k]])
-        ans.append(' '.join([i2c[i] for i in idx[:k]]))
-
-    return ans, ids
 
 train_data = pd.DataFrame()
 train_data['fname'] = train['fname']
@@ -106,8 +75,7 @@ clf = SVC(kernel = 'rbf', probability=True, C=10, gamma=0.01)
 
 clf.fit(X_train, y_train)
 hasil_prediksi = clf.predict(X_val)
-# print(accuracy_score(clf.predict(X_val), y_val))
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
 
 print(confusion_matrix(y_val, hasil_prediksi))
 print(classification_report(y_val, hasil_prediksi))
--- mfcc_extraction.py
+++ mfcc_extraction.py
@@ -1,33 +1,38 @@
-from python_speech_features import mfcc
-from python_speech_features import logfbank
-from python_speech_features import delta
-import scipy.io.wavfile as wav
-import numpy
-import os
 
-# directory where we your .wav files are
-directoryName = "dataset/senang/" # put your own directory here
-# directory to put our results in, you can change the name if you like
-resultsDirectory = directoryName + "/MFCCresults"
+import librosa
+import numpy as np
 
-# make a new folder in this directory to save our results in
-if not os.path.exists(resultsDirectory):
-    os.makedirs(resultsDirectory)
+import pandas as pd
+from scipy.stats import skew
 
-# get MFCCs for every .wav file in our specified directory
-for filename in os.listdir(directoryName):
-    if filename.endswith('.wav'): # only get MFCCs from .wavs
-        # read in our file
-        (rate,sig) = wav.read(directoryName + "/" +filename)
+SAMPLE_RATE = 44100
+def get_mfcc(name, path):
+    data, _ = librosa.core.load(path + name, sr = SAMPLE_RATE)
+    assert _ == SAMPLE_RATE
+    try:
+        ft1 = librosa.feature.mfcc(data, sr = SAMPLE_RATE, n_mfcc=30)
+        ft2 = librosa.feature.zero_crossing_rate(data)[0]
+        ft3 = librosa.feature.spectral_rolloff(data)[0]
+        ft4 = librosa.feature.spectral_centroid(data)[0]
+        ft5 = librosa.feature.spectral_contrast(data)[0]
+        ft6 = librosa.feature.spectral_bandwidth(data)[0]
+        ft1_trunc = np.hstack((np.mean(ft1, axis=1), np.std(ft1, axis=1), skew(ft1, axis = 1), np.max(ft1, axis = 1), np.median(ft1, axis = 1), np.min(ft1, axis = 1)))
+        ft2_trunc = np.hstack((np.mean(ft2), np.std(ft2), skew(ft2), np.max(ft2), np.median(ft2), np.min(ft2)))
+        ft3_trunc = np.hstack((np.mean(ft3), np.std(ft3), skew(ft3), np.max(ft3), np.median(ft3), np.min(ft3)))
+        ft4_trunc = np.hstack((np.mean(ft4), np.std(ft4), skew(ft4), np.max(ft4), np.median(ft4), np.min(ft4)))
+        ft5_trunc = np.hstack((np.mean(ft5), np.std(ft5), skew(ft5), np.max(ft5), np.median(ft5), np.min(ft5)))
+        ft6_trunc = np.hstack((np.mean(ft6), np.std(ft6), skew(ft6), np.max(ft6), np.median(ft6), np.max(ft6)))
+        return pd.Series(np.hstack((ft1_trunc, ft2_trunc, ft3_trunc, ft4_trunc, ft5_trunc, ft6_trunc)))
+    except:
+        print('bad file')
+        return pd.Series([0]*210)
 
-        # get mfcc
-        mfcc_feat = mfcc(sig,rate)
-        d_mfcc_feat = delta(mfcc_feat, 2)
-        # get filterbank energies
-        # fbank_feat = logfbank(sig,rate)
+def convert_to_labels(preds, i2c, k=3):
+    ans = []
+    ids = []
+    for p in preds:
+        idx = np.argsort(p)[::-1]
+        ids.append([i for i in idx[:k]])
+        ans.append(' '.join([i2c[i] for i in idx[:k]]))
 
-        # create a file to save our results in
-        outputFile = resultsDirectory + "/" + os.path.splitext(filename)[0] + ".csv"
-        file = open(outputFile, 'w+') # make file/over write existing file
-        numpy.savetxt(file, d_mfcc_feat, delimiter=",") #save MFCCs as .csv
-        file.close() # close file
+    return ans, ids
